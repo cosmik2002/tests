d537b322668961afebdcf711d685c95adce80616
Nicholas <2007.nicholas@gmail.com>
2017-12-22 18:00:15 +0300

    quit

--- qt_test.py
+++ qt_test.py
@@ -2,6 +2,7 @@ import sys
 from PyQt5.QtWidgets import (QWidget, QToolTip,
     QPushButton, QApplication)
 from PyQt5.QtGui import QFont,QIcon
+from PyQt5.QtCore import QCoreApplication
 
 class Example(QWidget):
     def __init__(self):
@@ -12,7 +13,8 @@ class Example(QWidget):
     def initUI(self):
          QToolTip.setFont(QFont('SansSerif',10))
          self.setToolTip('This is a <b>QWidget</b> widget')
-         btn = QPushButton('Button',self)
+         btn = QPushButton('Quit',self)
+         btn.clicked.connect(QCoreApplication.instance().quit)
          btn.setToolTip('This is a <b>QPushButton</b> widget')
          btn.resize(btn.sizeHint())
          btn.move(50,50)
